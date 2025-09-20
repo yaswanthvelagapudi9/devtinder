5dbfeb5e292c340d17786bcbabfc3687bb9fc45c
Yaswanth Velagapudi <yaswanth.velagapudi@gmail.com>
2025-09-20 14:49:32 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello World!");
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "John",
+    lastName: "Doe",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send({
+    message: "User created",
+  });
+});
+
+app.delete("/user", (req, res) => {
+  res.send({
+    message: "User deleted",
+  });
 });
 
 app.listen(3000, () => {
